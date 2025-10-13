07d8c2fadf1cbfe85888d28361a0d473c719f1c7
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-13 15:58:26 +0200

    ~run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-function f() {
-    ~/knuth_bendix_egraph/target/release/main -r "rules.rule" -t "data/$1" > o.txt
-}
+rm output.txt
 
 for x in $(ls data)
 do
     echo Starting with data/$x:
-    timeout 3s f $x
+    timeout 0.5s ~/knuth_bendix_egraph/target/release/main -r "rules.rule" -t "data/$x" > o.txt
+    t=$(cat o.txt | grep -A 1 "Extracted term" | tail -n 1)
+    hal="${x:5:1}"
+    if [[ "NUM$hal" == "$t" ]]; then
+        echo Success
+        echo Success >> output.txt
+    fi
+    echo
 done
